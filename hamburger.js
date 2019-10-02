3b986901b4a8cf314647b9286bcd349ae62a5ac5
suusan11 <gawow11@gmail.com>
2019-10-02 16:01:11 -0700

    add ease

--- hamburger.js
+++ hamburger.js
@@ -9,15 +9,19 @@ const tlm = new TimelineMax({});
 
 const toggleMenu = new TimelineMax({ paused: true, reversed: true });
 
-// hamburger.addEventListener('mouseenter', () => {
-//   tlm.staggerTo(lines, 1, { scaleX: 1.5, repeat: 1, yoyo: true }, 0.3);
-// })
+hamburger.addEventListener('mouseenter', () => {
+  if (hamburger.classList.contains('js-x')) {
+    return;
+  }
+  tlm.staggerTo(lines, 0.25, { scaleX: 1.5, repeat: 1, yoyo: true, ease: Power2.easeInOut }, 0.3);
+})
 
 toggleMenu
   .to(lineTwo, .125, { scaleX: 0 })
-  .to(lineOne, .125, { rotation: 45, transformOrigin: "50% 50%", y: 8 }, "cross")
-  .to(lineThree, .125, { rotation: -45, transformOrigin: "50% 50%", y: -8 }, "cross");
+  .to(lineOne, .125, { rotation: 45, transformOrigin: "50% 50%", y: 8, ease: Power2.easeInOut }, "cross")
+  .to(lineThree, .125, { rotation: -45, transformOrigin: "50% 50%", y: -8, ease: Power2.easeInOut }, "cross");
 
 hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('js-x');
   toggleMenu.reversed() ? toggleMenu.play() : toggleMenu.reverse();
 })
