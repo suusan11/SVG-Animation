15020a7927a5de3de1ff2658ef11bb9c0a0b7af5
suusan11 <gawow11@gmail.com>
2019-10-02 16:09:00 -0700

    add another animation

--- hamburger.js
+++ hamburger.js
@@ -13,13 +13,16 @@ hamburger.addEventListener('mouseenter', () => {
   if (hamburger.classList.contains('js-x')) {
     return;
   }
-  tlm.staggerTo(lines, 0.25, { scaleX: 1.5, repeat: 1, yoyo: true, ease: Power2.easeInOut }, 0.3);
+  tlm.staggerTo(lines, 0.25, { scaleX: 1.5, repeat: 1, yoyo: true, ease: Power2.easeInOut, svgOrigin: "50 50" }, 0.125);
 })
 
 toggleMenu
   .to(lineTwo, .125, { scaleX: 0 })
-  .to(lineOne, .125, { rotation: 45, transformOrigin: "50% 50%", y: 8, ease: Power2.easeInOut }, "cross")
-  .to(lineThree, .125, { rotation: -45, transformOrigin: "50% 50%", y: -8, ease: Power2.easeInOut }, "cross");
+  .to(lineOne, .25, { transformOrigin: "50% 50%", y: 8, ease: Power2.easeInOut }, "slide")
+  .to(lineThree, .25, { transformOrigin: "50% 50%", y: -8, ease: Power2.easeInOut }, "slide")
+  .to(hamburger, .5, { rotation: 360, ease: Power4.easeInOut })
+  .to(lineOne, .25, { rotation: 45, ease: Power2.easeInOut }, "cross")
+  .to(lineThree, .25, { rotation: -45, ease: Power2.easeInOut }, "cross");
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('js-x');
